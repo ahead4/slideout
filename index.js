a7fcc909a04b5f2567126593b2024dd8cecc0b29
Sladewill <Sladewill@users.noreply.github.com>
2016-11-04 09:40:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -75,12 +75,23 @@ function Slideout(options) {
   this._duration = parseInt(options.duration, 10) || 300;
   this._tolerance = parseInt(options.tolerance, 10) || 70;
   this._padding = this._translateTo = parseInt(options.padding, 10) || 256;
+  this._closeOnClick = options.closeOnClick || false;
+  this._grabWidth = parseInt(options.grabWidth, 10) || 0;
   this._orientation = options.side === 'right' ? -1 : 1;
   this._translateTo *= this._orientation;
 
   // Init touch events
   if (this._touch) {
     this._initTouchEvents();
+  }
+  
+  if (this._closeOnClick) {
+    /**
+     * Close menu when panel is clicked while open
+     */
+    this.panel.addEventListener('click', function() {
+      if (self.isOpen) { self.close(); }
+    });
   }
 }
 
@@ -197,8 +208,9 @@ Slideout.prototype._initTouchEvents = function() {
 
     self._moved = false;
     self._opening = false;
-    self._startOffsetX = eve.touches[0].pageX;
-    self._preventOpen = (!self._touch || (!self.isOpen() && self.menu.clientWidth !== 0));
+    var offset = eve.touches[0].pageX;
+    self._startOffsetX = offset;
+    self._preventOpen = (!self.isOpen() && (self.menu.clientWidth !== 0 || (self._grabWidth && offset > self._grabWidth)));
   };
 
   this.panel.addEventListener(touch.start, this._resetTouchFn);
